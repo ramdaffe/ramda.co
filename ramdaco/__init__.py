37c24205097b8b0be3123dcbac9999191b60dd2d
Ramda Yanurzha <ramda.yz@gmail.com>
2016-04-24 03:13:47 -0400

    trying mail logic

--- ramdaco/__init__.py
+++ ramdaco/__init__.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def hello():
 	return render_template('index.html')
 
+
+@app.route("/hi", methods=["POST"])
+def mailin():
+    is_spam = request.form['X-Mailgun-SFlag'] == 'Yes'
+    return request.form['subject']
+
 if __name__ == "__main__":
     app.run()
